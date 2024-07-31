302b447cea036e59a80023aa0151f41de520f958
Evfidiw <evfxie@126.com>
2024-07-31 11:08:47 +0800

    update scripts

--- scripts/2train.sh
+++ scripts/2train.sh
@@ -2,7 +2,7 @@ cd "$(dirname "$0")"/..
 
 python3 main.py \
 --device 0 \
---model_path "model_weight/clip-vit-large-patch14" \
+--model_path "openai/clip-vit-large-patch14" \
 --fusion 'att' \
 --train_batch_size 64 \
 --dev_batch_size 64 \
@@ -12,7 +12,7 @@ python3 main.py \
 --num_experts 4 \
 --layers 3 \
 --num_validation_steps 2000 \
---early_stop 1 \
+--early_stop 5 \
 --num_train_epochs 10 \
 --text_name text_json_final \
 --train \
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,28 +1,19 @@
 cd "$(dirname "$0")"/..
 
 python3 main.py \
---model CLIP \
---fusion att \
---weight_decay 0.05 \
+--device 0 \
+--model_path "openai/clip-vit-large-patch14" \
+--fusion 'att' \
 --train_batch_size 64 \
 --dev_batch_size 64 \
---learning_rate 5e-4 \
---clip_learning_rate 1e-6 \
---layers 3 \
---max_grad_norm 5 \
---dropout_rate 0.1 \
---optimizer_name adam \
---text_size 512 \
+--text_size 768 \
 --image_size 768 \
---warmup_proportion 0.2 \
---device 0 \
---device 0 \
 --seed 3407 \
---num_experts 8 \
+--num_experts 4 \
 --layers 3 \
 --num_validation_steps 2000 \
 --early_stop 5 \
---num_train_epochs 5 \
+--num_train_epochs 10 \
 --text_name text_json_clean \
 --train \
 --test \
